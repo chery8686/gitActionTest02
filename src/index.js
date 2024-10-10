f41f6e23ed3af50c9c279204033d2f8a6eab8033
KCH <kek6kr1@naver.com>
2024-10-10 21:15:48 +0900

    breack-test: n<0부분이 삭제 되었음

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 //version: 1 -> 2 -> 3
 
 function factorial(n) {
-
+/*
   if( n < 0 ) {
       throw new Error('n must be a positive integer');
   }
+*/
 
-  
   if( n === 0 ) {
       return 1;
   }
